493080eb75551d52d07fbb7a38ad5c99c5f37900
Wesley Porter <wesp@qualtrics.com>
2019-12-12 11:08:17 -0700

    Complete part 2 of day 12

--- internal/common/math/math.go
+++ internal/common/math/math.go
@@ -36,3 +36,24 @@ func ToFixed(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
 	return float64(Round(num*output)) / output
 }
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
