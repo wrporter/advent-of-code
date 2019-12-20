155df6b120f848143e60b9ebb6ac56adb1c8cee0
Wesley Porter <wesp@qualtrics.com>
2019-12-20 14:02:24 -0700

    Complete part 2 of day20

--- day20/internal/maze/maze.go
+++ day20/internal/maze/maze.go
@@ -17,15 +17,23 @@ const (
 
 type DonutMaze struct {
 	maze    map[geometry.Point]byte
-	portals map[geometry.Point]geometry.Point
+	portals map[geometry.Point]Portal
 	start   geometry.Point
 	end     geometry.Point
 }
 
+type Portal struct {
+	name  string
+	to    geometry.Point
+	level int
+}
+
+const MaxDepth = 100
+
 func New(maze []string) *DonutMaze {
 	mazeMap := make(map[geometry.Point]byte)
 	portals := make(map[string]geometry.Point)
-	connections := make(map[geometry.Point]geometry.Point)
+	connections := make(map[geometry.Point]Portal)
 
 	for y, line := range maze {
 		for x, char := range line {
@@ -42,8 +50,8 @@ func New(maze []string) *DonutMaze {
 
 					name := string(maze[y][x]) + string(maze[y+1][x])
 					if _, ok := portals[name]; ok {
-						connections[portals[name]] = passage
-						connections[passage] = portals[name]
+						connections[portals[name]] = buildPortal(maze, name, portals[name], passage)
+						connections[passage] = buildPortal(maze, name, passage, portals[name])
 					} else {
 						portals[name] = passage
 					}
@@ -55,8 +63,8 @@ func New(maze []string) *DonutMaze {
 
 					name := string(maze[y][x]) + string(maze[y][x+1])
 					if _, ok := portals[name]; ok {
-						connections[portals[name]] = passage
-						connections[passage] = portals[name]
+						connections[portals[name]] = buildPortal(maze, name, portals[name], passage)
+						connections[passage] = buildPortal(maze, name, passage, portals[name])
 					} else {
 						portals[name] = passage
 					}
@@ -73,41 +81,64 @@ func New(maze []string) *DonutMaze {
 	}
 }
 
+func buildPortal(maze []string, name string, portal geometry.Point, to geometry.Point) Portal {
+	level := 1
+	if portal.Y == 2 ||
+		portal.Y == len(maze)-3 ||
+		portal.X == 2 ||
+		portal.X == len(maze[portal.Y])-3 {
+		level = -1
+	}
+	return Portal{name, to, level}
+}
+
 type node struct {
 	point geometry.Point
 	steps int
+	level int
 }
 
-func (m *DonutMaze) MinSteps() int {
-	queue := []node{{m.start, 0}}
-	visited := make(map[geometry.Point]bool)
+func (m *DonutMaze) MinSteps(recursive bool) int {
+	queue := []node{{m.start, 0, 0}}
+	visited := make(map[Neighbor]bool)
 	var cur node
 
 	for len(queue) > 0 {
 		cur, queue = queue[0], queue[1:]
-		if cur.point == m.end {
+		visited[Neighbor{cur.point, cur.level}] = true
+
+		if cur.point == m.end && cur.level == 0 {
 			return cur.steps
 		}
-		for _, neighbor := range m.getNeighbors(cur.point, visited) {
-			queue = append(queue, node{neighbor, cur.steps + 1})
+
+		for _, neighbor := range m.getNeighbors(cur, visited, recursive) {
+			if neighbor.level >= 0 && neighbor.level < MaxDepth && !visited[neighbor] {
+				queue = append(queue, node{neighbor.point, cur.steps + 1, neighbor.level})
+			}
 		}
-		visited[cur.point] = true
 	}
 
-	panic("Failed to find a route!")
+	return -1
 }
 
-func (m *DonutMaze) getNeighbors(point geometry.Point, visited map[geometry.Point]bool) []geometry.Point {
-	var neighbors []geometry.Point
-	if portal, ok := m.portals[point]; ok {
-		neighbors = append(neighbors, portal)
+type Neighbor struct {
+	point geometry.Point
+	level int
+}
+
+func (m *DonutMaze) getNeighbors(cur node, visited map[Neighbor]bool, recursive bool) []Neighbor {
+	var neighbors []Neighbor
+	if portal, ok := m.portals[cur.point]; ok {
+		level := 0
+		if recursive {
+			level = cur.level + portal.level
+		}
+		neighbors = append(neighbors, Neighbor{portal.to, level})
 	}
 	for _, direction := range geometry.Directions {
-		p := point.Add(direction)
-		if !visited[p] {
-			if m.maze[p] == OpenPassage {
-				neighbors = append(neighbors, p)
-			}
+		p := cur.point.Add(direction)
+		if m.maze[p] == OpenPassage {
+			neighbors = append(neighbors, Neighbor{p, cur.level})
 		}
 	}
 	return neighbors
--- day20/main.go
+++ day20/main.go
@@ -5,82 +5,24 @@ import (
 	"github.com/wrporter/advent-of-code-2019/day20/internal/maze"
 	"github.com/wrporter/advent-of-code-2019/internal/common/file"
 	"github.com/wrporter/advent-of-code-2019/internal/common/timeit"
-	"strings"
 	"time"
 )
 
 func main() {
-	runSampleMaze(`         A           
-         A           
-  #######.#########  
-  #######.........#  
-  #######.#######.#  
-  #######.#######.#  
-  #######.#######.#  
-  #####  B    ###.#  
-BC...##  C    ###.#  
-  ##.##       ###.#  
-  ##...DE  F  ###.#  
-  #####    G  ###.#  
-  #########.#####.#  
-DE..#######...###.#  
-  #.#########.###.#  
-FG..#########.....#  
-  ###########.#####  
-             Z       
-             Z       `)
-	runSampleMaze(`
-"                   A               "
-"                   A               "
-"  #################.#############  "
-"  #.#...#...................#.#.#  "
-"  #.#.#.###.###.###.#########.#.#  "
-"  #.#.#.......#...#.....#.#.#...#  "
-"  #.#########.###.#####.#.#.###.#  "
-"  #.............#.#.....#.......#  "
-"  ###.###########.###.#####.#.#.#  "
-"  #.....#        A   C    #.#.#.#  "
-"  #######        S   P    #####.#  "
-"  #.#...#                 #......VT"
-"  #.#.#.#                 #.#####  "
-"  #...#.#               YN....#.#  "
-"  #.###.#                 #####.#  "
-"DI....#.#                 #.....#  "
-"  #####.#                 #.###.#  "
-"ZZ......#               QG....#..AS"
-"  ###.###                 #######  "
-"JO..#.#.#                 #.....#  "
-"  #.#.#.#                 ###.#.#  "
-"  #...#..DI             BU....#..LF"
-"  #####.#                 #.#####  "
-"YN......#               VT..#....QG"
-"  #.###.#                 #.###.#  "
-"  #.#...#                 #.....#  "
-"  ###.###    J L     J    #.#.###  "
-"  #.....#    O F     P    #.#...#  "
-"  #.###.#####.#.#####.#####.###.#  "
-"  #...#.#.#...#.....#.....#.#...#  "
-"  #.#####.###.###.#.#.#########.#  "
-"  #...#.#.....#...#.#.#.#.....#.#  "
-"  #.###.#####.###.###.#.#.#######  "
-"  #.#.........#...#.............#  "
-"  #########.###.###.#############  "
-"           B   J   C               "
-"           U   P   P               "
-`)
-	runMaze("./day20/input.txt")
+	runMaze("./day20/sample1.txt", false)
+	runMaze("./day20/sample2.txt", false)
+	runMaze("./day20/sample3.txt", false)
+	runMaze("./day20/input.txt", false)
+
+	runMaze("./day20/sample1.txt", true)
+	runMaze("./day20/sample2.txt", true)
+	runMaze("./day20/sample3.txt", true)
+	runMaze("./day20/input.txt", true)
 }
 
-func runSampleMaze(input string) {
-	lines := strings.Split(input, "\n")
-	defer timeit.Track(time.Now(), "find ZZ")
-	m := maze.New(lines)
-	fmt.Println(m.MinSteps())
-}
-
-func runMaze(inputFile string) {
+func runMaze(inputFile string, recursive bool) {
 	lines, _ := file.ReadFile(inputFile)
-	defer timeit.Track(time.Now(), "find ZZ")
+	defer timeit.Track(time.Now(), fmt.Sprintf("run maze (rec: %v): %s", recursive, inputFile))
 	m := maze.New(lines)
-	fmt.Println(m.MinSteps())
+	fmt.Println(m.MinSteps(recursive))
 }
