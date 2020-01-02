abe199d9236da596ec26844c02d81005ca4b6394
Wesley Porter <wesp@qualtrics.com>
2020-01-01 21:32:03 -0700

    Finish part2 of day5 of 2015

---
+++ 2015/day6/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wrporter/advent-of-code/internal/common/conversion"
+	"github.com/wrporter/advent-of-code/internal/common/file"
+	"regexp"
+)
+
+type instructionCommand string
+
+const (
+	on     instructionCommand = "turn on"
+	toggle instructionCommand = "toggle"
+	off    instructionCommand = "turn off"
+)
+
+type instruction struct {
+	command  instructionCommand
+	startCol int
+	startRow int
+	endCol   int
+	endRow   int
+}
+
+var regex = regexp.MustCompile(`^(turn on|toggle|turn off) (\d+),(\d+) through (\d+),(\d+)$`)
+
+func setupLights(instructions []string) int {
+	grid := [1000][1000]bool{}
+
+	for _, instructionString := range instructions {
+		match := regex.FindStringSubmatch(instructionString)
+		ins := instruction{
+			command:  instructionCommand(match[1]),
+			startCol: conversion.StringToInt(match[2]),
+			startRow: conversion.StringToInt(match[3]),
+			endCol:   conversion.StringToInt(match[4]),
+			endRow:   conversion.StringToInt(match[5]),
+		}
+
+		if ins.command == on {
+			for row := ins.startRow; row <= ins.endRow; row++ {
+				for col := ins.startCol; col <= ins.endCol; col++ {
+					grid[row][col] = true
+				}
+			}
+		} else if ins.command == off {
+			for row := ins.startRow; row <= ins.endRow; row++ {
+				for col := ins.startCol; col <= ins.endCol; col++ {
+					grid[row][col] = false
+				}
+			}
+		} else if ins.command == toggle {
+			for row := ins.startRow; row <= ins.endRow; row++ {
+				for col := ins.startCol; col <= ins.endCol; col++ {
+					grid[row][col] = !grid[row][col]
+				}
+			}
+		}
+	}
+
+	numLightsOn := 0
+	for row := range grid {
+		for _, light := range grid[row] {
+			if light {
+				numLightsOn++
+			}
+		}
+	}
+	return numLightsOn
+}
+
+func main() {
+	instructions, _ := file.ReadFile("./2015/day6/input.txt")
+	fmt.Println(setupLights(instructions))
+}
---
+++ 2015/day6/readme.md
@@ -0,0 +1,5 @@
+## Day 6
+
+### Part 1
+
+Ah, now this one looks more interesting, having to parse some instructions. I use a regex and store the values then act on each command `turn on`, `toggle`, and `turn off`. I started off with a fast approach and plan to clean it up with Part 2.
