1fd92a3967316bfdb17c088c8b68f918074466b1
Wesley Porter <wesp@qualtrics.com>
2020-01-07 19:51:45 -0700

    Finish on part2 of day12 of 2015

--- 2015/day12/main.go
+++ 2015/day12/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/wrporter/advent-of-code/internal/common/conversion"
 	"github.com/wrporter/advent-of-code/internal/common/file"
@@ -16,7 +17,40 @@ func sum(json string) int {
 	return ints.Sum(numbers)
 }
 
+func SumRed(data string) int {
+	var container interface{}
+	_ = json.Unmarshal([]byte(data), &container)
+	return sumRed(container)
+}
+
+func sumRed(object interface{}) int {
+	sum := 0
+
+	switch cur := object.(type) {
+	case float64:
+		sum += int(cur)
+	case []interface{}:
+		for i := 0; i < len(cur); i++ {
+			sum += sumRed(cur[i])
+		}
+	case map[string]interface{}:
+		potentialSum := 0
+		for _, value := range cur {
+			if value == "red" {
+				potentialSum = 0
+				break
+			} else {
+				potentialSum += sumRed(value)
+			}
+		}
+		sum += potentialSum
+	}
+
+	return sum
+}
+
 func main() {
 	lines, _ := file.ReadFile("./2015/day12/input.txt")
 	fmt.Println(sum(lines[0]))
+	fmt.Println(SumRed(lines[0]))
 }
--- 2015/day12/readme.md
+++ 2015/day12/readme.md
@@ -7,3 +7,5 @@ Oh nice, I don't even have to parse the JSON! I can just use a regular expressio
 ### Part 2
 
 Okay, I just had to drop my idea of doing **everything** in Go, because now I had to parse the JSON. In just the time (about 10 minutes) it took me to look at how I might go about this in a decently readable way in Go, I wrote the solution in JavaScript. Maybe I'll look at this again in Go, I just didn't think it was worth the time.
+
+Ah, okay, that really wasn't so bad. I wasn't sure if Go would allow me to determine the difference between a generic object and an array, but it turns out that their object for JSON is a `map[string]interface{}`. My code is almost exactly the same as the JavaScript version.
