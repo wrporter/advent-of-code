67a1e9b9ef46ad990ac88b191035dbb0dc785426
Wesley Porter <wesleyrporter@gmail.com>
2020-12-04 07:25:53 -0700

    Overall bash runner now works for a given year

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-find 2020 -name "main.go" -maxdepth 2 -print0 | sort --version-sort -z | xargs -0 -I{} go run {}
+year=${1:-$(date +"%Y")}
+: "${year:?Must pass the year}"
+
+echo "--- Executing files for year: ${year}"
+echo
+
+find "${year}" -name "main.go" -maxdepth 2 -print0 | sort --version-sort -z | xargs -0 -I{} sh -c "go run {}; echo"
