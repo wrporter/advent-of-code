3498d96247e3fe962219bc5e3674d9a2ef41f4a1
Wesley Porter <wesleyrporter@gmail.com>
2020-12-09 23:45:46 -0700

    Completed 2020 day10

---
+++ 2020/day10/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wrporter/advent-of-code/internal/common/conversion"
+	"github.com/wrporter/advent-of-code/internal/common/file"
+	"github.com/wrporter/advent-of-code/internal/common/ints"
+	"github.com/wrporter/advent-of-code/internal/common/out"
+	"sort"
+)
+
+func main() {
+	year, day := 2020, 10
+	out.Day(year, day)
+	input, _ := file.ReadFile(fmt.Sprintf("./%d/day%d/sample-input2.txt", year, day))
+
+	answer1 := part1(input)
+	out.Part1(answer1)
+
+	answer2 := part2(input)
+	out.Part2(answer2)
+}
+
+func part1(input []string) interface{} {
+	adapters, _ := conversion.ToInts(input)
+	sort.Ints(adapters)
+
+	builtInAdapter := getBuiltInAdapter(adapters)
+	result, _ := jolt(adapters, builtInAdapter)
+
+	return result[1] * result[3]
+}
+
+func part2(input []string) interface{} {
+	adapters, _ := conversion.ToInts(input)
+	sort.Ints(adapters)
+	builtInAdapter := getBuiltInAdapter(adapters)
+	adapters = append(adapters, builtInAdapter)
+
+	// Dynamic Programming memoization
+	combinations := map[int]int{0: 1}
+	for _, adapter := range adapters {
+		combinations[adapter] = combinations[adapter-1] + combinations[adapter-2] + combinations[adapter-3]
+	}
+
+	// Brute force way that takes too long for large data sets
+	//count := 0
+	//probability.ComboSize(adapters, 1, len(adapters), func(i []int) {
+	//	if _, passed := jolt(i, builtInAdapter); passed {
+	//		count++
+	//	}
+	//})
+	//return count
+
+	return combinations[adapters[len(adapters)-1]]
+}
+
+func getBuiltInAdapter(adapter []int) int {
+	builtInAdapter := 0
+	for _, rating := range adapter {
+		builtInAdapter = ints.Max(builtInAdapter, rating)
+	}
+	return builtInAdapter + 3
+}
+
+func jolt(adapters []int, builtInAdapter int) (map[int]int, bool) {
+	adapters = append(adapters, builtInAdapter)
+
+	result := make(map[int]int)
+	j := 0
+	for _, rating := range adapters {
+		diff := ints.Abs(j - rating)
+		if diff <= 3 {
+			result[diff]++
+			j = rating
+		} else {
+			return nil, false
+		}
+	}
+	return result, true
+}
