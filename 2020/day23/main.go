30d5bd3774bc9a565da04bdce6c3c0c967006ea9
Wesley Porter <wesleyrporter@gmail.com>
2020-12-23 07:50:46 -0700

    Part 1 2020 day 23

--- 2020/day23/main.go
+++ 2020/day23/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"github.com/wrporter/advent-of-code/internal/common/conversion"
 	"github.com/wrporter/advent-of-code/internal/common/file"
+	"github.com/wrporter/advent-of-code/internal/common/ints"
 	"github.com/wrporter/advent-of-code/internal/common/out"
 	"github.com/wrporter/advent-of-code/internal/common/timeit"
 	"strings"
@@ -15,7 +17,7 @@ func main() {
 
 	year, day := 2020, 23
 	out.Day(year, day)
-	input, _ := file.ReadFile(fmt.Sprintf("./%d/day%d/sample-input.txt", year, day))
+	input, _ := file.ReadFile(fmt.Sprintf("./%d/day%d/input.txt", year, day))
 
 	answer1 := part1(input)
 	out.Part1(answer1)
@@ -26,43 +28,142 @@ func main() {
 
 func part1(input []string) interface{} {
 	cups := parse(input)
-
-	play(cups, 10)
-
-	return 0
+	cups = play(cups, 100)
+	return labelsAfter1(cups)
 }
 
-func play(cups []int, moves int) []int {
-	current := 0
-	var pickup [3]int
+func labelsAfter1(cups *list.List) string {
+	var sb strings.Builder
+	initial := get(cups, 1)
+
+	for e := initial.Next(); e != initial; e = e.Next() {
+		if e == nil {
+			e = cups.Front()
+		}
+		_, _ = fmt.Fprintf(&sb, "%d", e.Value)
+	}
+
+	return sb.String()
+}
+
+func play(cups *list.List, moves int) *list.List {
+	current := cups.Front()
 
 	for move := 1; move <= moves; move++ {
-		fmt.Printf("-- move %d --\n", move)
-		fmt.Printf("cups: %s\n", render(cups, current))
+		//fmt.Printf("-- move %d --\n", move)
+		//fmt.Printf("cups: %s\n", render(cups, current))
 
+		pickup := list.New()
 		for pick := 0; pick < 3; pick++ {
-			cupPosition := (current + 1 + pick) % len(cups)
-			// pickup the cups
-			pickup[pick] = cups[cupPosition]
-			// shift the cups to fill in the space that was taken
-			cups[cupPosition] = cups[(current+4)%len(cups)]
+			remove := current.Next()
+			if remove == nil {
+				remove = cups.Front()
+			}
+			pickup.PushBack(cups.Remove(remove))
+		}
+		//fmt.Printf("pickup: %s\n", renderList(pickup))
+
+		destination := current
+		destinationCup := getDestinationCup(cups, pickup, current)
+		for destination == nil || destination.Value != destinationCup {
+			if destination == nil {
+				destination = cups.Front()
+			} else {
+				destination = destination.Next()
+			}
+		}
+		//fmt.Printf("destination: %d\n\n", destination.Value)
+
+		for cup := pickup.Back(); cup != nil; cup = cup.Prev() {
+			cups.InsertAfter(cup.Value, destination)
+		}
+
+		current = current.Next()
+		if current == nil {
+			current = cups.Front()
 		}
 	}
+
+	//fmt.Printf("-- final --\n")
+	//fmt.Printf("cups: %s\n", render(cups, current))
 
 	return cups
 }
 
-func render(cups []int, current int) string {
+func getDestinationCup(cups *list.List, pickup *list.List, current *list.Element) int {
+	high := highest(cups)
+	low := lowest(cups)
+	destination := current.Value.(int) - 1
+
+	for contains(pickup, destination) || !contains(cups, destination) {
+		destination--
+		if destination < low {
+			return high
+		}
+	}
+
+	return destination
+}
+
+func contains(l *list.List, value int) bool {
+	for e := l.Front(); e != nil; e = e.Next() {
+		if e.Value == value {
+			return true
+		}
+	}
+	return false
+}
+
+func get(l *list.List, value int) *list.Element {
+	for e := l.Front(); e != nil; e = e.Next() {
+		if e.Value == value {
+			return e
+		}
+	}
+	return nil
+}
+
+func highest(cups *list.List) int {
+	max := 0
+	for e := cups.Front(); e != nil; e = e.Next() {
+		max = ints.Max(max, e.Value.(int))
+	}
+	return max
+}
+
+func lowest(cups *list.List) int {
+	min := ints.MaxInt
+	for e := cups.Front(); e != nil; e = e.Next() {
+		min = ints.Min(min, e.Value.(int))
+	}
+	return min
+}
+
+func renderList(l *list.List) interface{} {
 	var sb strings.Builder
 	delimiter := ' '
 
-	for i, cup := range cups {
-		if i == current {
-			sb.WriteString(fmt.Sprintf("(%d)", cup))
-		} else {
-			sb.WriteString(fmt.Sprintf("%d", cup))
+	for e := l.Front(); e != nil; e = e.Next() {
+		sb.WriteString(fmt.Sprintf("%d", e.Value))
+		if e != l.Back() {
+			sb.WriteRune(delimiter)
 		}
-		if i < len(cups)-1 {
+	}
+
+	return sb.String()
+}
+
+func render(cups *list.List, current *list.Element) string {
+	var sb strings.Builder
+	delimiter := ' '
+
+	for cup := cups.Front(); cup != nil; cup = cup.Next() {
+		if cup == current {
+			sb.WriteString(fmt.Sprintf("(%d)", cup.Value))
+		} else {
+			sb.WriteString(fmt.Sprintf("%d", cup.Value))
+		}
+		if cup != cups.Back() {
 			sb.WriteRune(delimiter)
 		}
 	}
@@ -74,8 +175,12 @@ func part2(input []string) interface{} {
 	return 0
 }
 
-func parse(input []string) []int {
+func parse(input []string) *list.List {
 	labels := strings.Split(input[0], "")
-	cups, _ := conversion.ToInts(labels)
+	values, _ := conversion.ToInts(labels)
+	cups := list.New()
+	for _, value := range values {
+		cups.PushBack(value)
+	}
 	return cups
 }
