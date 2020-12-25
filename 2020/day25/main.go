3f24be134bd8279817cc8c89aa6b54116cd6774f
Wesley Porter <wesleyrporter@gmail.com>
2020-12-25 06:02:04 -0700

    Completed 2020 day 25

---
+++ 2020/day25/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wrporter/advent-of-code/internal/common/conversion"
+	"github.com/wrporter/advent-of-code/internal/common/file"
+	"github.com/wrporter/advent-of-code/internal/common/out"
+	"github.com/wrporter/advent-of-code/internal/common/timeit"
+	"time"
+)
+
+func main() {
+	defer timeit.Report(time.Now())
+
+	year, day := 2020, 25
+	out.Day(year, day)
+	input, _ := file.ReadFile(fmt.Sprintf("./%d/day%d/input.txt", year, day))
+
+	answer1 := part1(input)
+	out.Part1(answer1)
+
+	answer2 := part2(input)
+	out.Part2(answer2)
+}
+
+func part1(input []string) interface{} {
+	publicKeys, _ := conversion.ToInts(input)
+	cardPublicKey := publicKeys[0]
+	doorPublicKey := publicKeys[1]
+
+	subjectNumber := 7
+	cardLoopSize := findLoopSize(cardPublicKey, subjectNumber)
+	doorLoopSize := findLoopSize(doorPublicKey, subjectNumber)
+
+	cardEncryptionKey := getEncryptionKey(cardPublicKey, doorLoopSize)
+	doorEncryptionKey := getEncryptionKey(doorPublicKey, cardLoopSize)
+
+	if cardEncryptionKey == doorEncryptionKey {
+		return cardEncryptionKey
+	}
+
+	return 0
+}
+
+func findLoopSize(key int, subjectNumber int) int {
+	value := 1
+	for loopSize := 1; loopSize < 1_000_000_000; loopSize++ {
+		value *= subjectNumber
+		value %= 20201227
+
+		if value == key {
+			return loopSize
+		}
+	}
+	return 0
+}
+
+func getEncryptionKey(subjectNumber int, loopSize int) int {
+	value := 1
+	for loop := 1; loop <= loopSize; loop++ {
+		value *= subjectNumber
+		value %= 20201227
+	}
+	return value
+}
+
+func part2(input []string) interface{} {
+	return "Merry Christmas!!!"
+}
