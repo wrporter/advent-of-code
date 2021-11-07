ffacbf726a74418d4bbee4d09848bbe6a2405e40
Wesley Porter <wesleyrporter@gmail.com>
2021-11-06 20:51:29 -0600

    2017 day 5

---
+++ 2017/day5/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_part1(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  interface{}
+	}{
+		{
+			[]string{
+				"0",
+				"3",
+				"0",
+				"1",
+				"-3",
+			},
+			5,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("Test_%d", i), func(t *testing.T) {
+			if got := part1(tt.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
