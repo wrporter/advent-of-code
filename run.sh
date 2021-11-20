28d2fb70a6eba2fb42f4552db1d2584b9e3a1672
Wesley Porter <wesleyrporter@gmail.com>
2021-11-20 12:26:17 -0700

    2017 day 25

--- run.sh
+++ run.sh
@@ -3,7 +3,12 @@
 year=${1:-$(date +"%Y")}
 : "${year:?Must pass the year}"
 
-echo "--- Executing files for year: ${year}"
+green='\033[1;92m'
+reset='\033[0m'
+
+echo -e "${green}===============================${reset}"
+echo -e "${green}= Saving Santa for year: ${year} =${reset}"
+echo -e "${green}===============================${reset}"
 echo
 
 find "${year}" -name "main.go" -maxdepth 2 -print0 | sort --version-sort -z | xargs -0 -I{} sh -c "go run {}; echo"
