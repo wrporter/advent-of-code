adca0b1630b21a391c8ddedd4de7eff8f11a3165
Wesley Porter <wesleyrporter@gmail.com>
2021-11-28 19:59:12 -0700

    2018 day 7 part 2 incorrect but close

--- 2018/day7/main.go
+++ 2018/day7/main.go
@@ -46,7 +46,82 @@ func part1(input []string) interface{} {
 }
 
 func part2(input []string) interface{} {
-	return 0
+	numWorkers := 5
+	baseTime := 60
+	dependencies := parseInput(input)
+	remaining := make(map[rune]bool)
+	for step := range dependencies {
+		remaining[step] = true
+	}
+
+	done := make(map[rune]bool)
+	workers := newWorkers(numWorkers)
+	inProgress := make(map[rune]bool)
+	totalTime := -1
+	for len(remaining) > 0 {
+		nextWorker := getNextWorker(workers)
+		if nextWorker != nil {
+			next := getNextStepForWorker(remaining, dependencies, done, inProgress)
+			if next != 0 && !inProgress[next] {
+				nextWorker.step = next
+				nextWorker.time = baseTime + int(next-'A') + 1
+				inProgress[next] = true
+			}
+		}
+		for _, w := range workers {
+			if w.step != 0 {
+				w.time--
+				if w.time == 0 {
+					done[w.step] = true
+					inProgress[w.step] = true
+					delete(remaining, w.step)
+					w.step = 0
+				}
+			}
+		}
+		if len(remaining) == 0 {
+			break
+		}
+		totalTime++
+	}
+
+	return totalTime
+}
+
+func getNextStepForWorker(remaining map[rune]bool, dependencies map[rune]map[rune]bool, done map[rune]bool, inProgress map[rune]bool) rune {
+	var nextSteps []rune
+	for step := range remaining {
+		if available(dependencies, done, step) && !inProgress[step] {
+			nextSteps = append(nextSteps, step)
+		}
+	}
+	runes.Sort(nextSteps)
+	if len(nextSteps) == 0 {
+		return 0
+	}
+	return nextSteps[0]
+}
+
+func getNextWorker(workers []*worker) *worker {
+	for _, w := range workers {
+		if w.time == 0 {
+			return w
+		}
+	}
+	return nil
+}
+
+func newWorkers(numWorkers int) []*worker {
+	var workers []*worker
+	for i := 0; i < numWorkers; i++ {
+		workers = append(workers, &worker{})
+	}
+	return workers
+}
+
+type worker struct {
+	step rune
+	time int
 }
 
 func getNextStep(remaining map[rune]bool, dependencies map[rune]map[rune]bool, done map[rune]bool) rune {
