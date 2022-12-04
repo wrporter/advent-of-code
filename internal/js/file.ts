0dcafe32028b1868b1ff44c2d93a26202fc36e91
Wesley Porter <wesp@qualtrics.com>
2022-12-03 20:26:15 -0700

    Reorganize

--- internal/js/file.ts
+++ internal/js/file.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
 
 export function read(year: number, day: number, filename: string): string {
-    return fs.readFileSync(`${year}/day${day}/${filename}`, 'utf8');
+    return fs.readFileSync(`solutions/${year}/day${day}/${filename}`, 'utf8');
 }
