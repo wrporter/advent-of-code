1a2314da282628ce58657326d7cf05ca2cee110b
Wesley Porter <wesp@qualtrics.com>
2022-12-11 07:11:12 -0700

    2022 day 11 clean up

--- solutions/2022/day11/solution.ts
+++ solutions/2022/day11/solution.ts
@@ -6,93 +6,94 @@ export class Solution extends AbstractSolution {
     filename = 'input.txt';
 
     part1(input: string): string | number {
-        return calculateMonkeyBusiness(input, 20, 3);
+        const monkeys = parseMonkeyStates(input);
+        const manageWorry = (worry: number) => Math.floor(worry / 3);
+        return calculateMonkeyBusiness(monkeys, 20, manageWorry);
     }
 
     part2(input: string): string | number {
-        return calculateMonkeyBusiness(input, 10_000);
+        const monkeys = parseMonkeyStates(input);
+
+        // Chinese Remainder Theorem: Get product of all prime moduli
+        const modulus = monkeys.reduce((product, monkey) => product * monkey.test.divisibleBy, 1);
+
+        const manageWorry = (worry: number) => worry % modulus;
+        return calculateMonkeyBusiness(monkeys, 10_000, manageWorry);
     }
 }
 
-// const startingItemsRegex = /^  Starting items: (\d+(?:, \d+)*)$/;
-const START = '  Starting items: ';
-const OPERATION = '  Operation: new = old ';
+function calculateMonkeyBusiness(monkeys: Monkey[], maxRounds: number, manageWorry: (worry: number) => number) {
+    for (let round = 0; round < maxRounds; round++) {
+        monkeys.forEach((monkey) => {
+            if (monkey.items.length === 0) {
+                return; // skip monkeys with no items
+            }
 
-function calculateMonkeyBusiness(input: string, maxRounds: number, worryDivider?: number) {
+            const { divisibleBy, trueMonkey, falseMonkey } = monkey.test;
+            monkey.items.forEach((item) => {
+                monkey.numItemsInspected += 1;
+
+                let worry = monkey.operation(item);
+                worry = manageWorry(worry);
+
+                if (worry % divisibleBy === 0) {
+                    monkeys[trueMonkey].items.push(worry);
+                } else {
+                    monkeys[falseMonkey].items.push(worry);
+                }
+            });
+            monkey.items = [];
+        });
+    }
+
+    monkeys.sort((a, b) => b.numItemsInspected - a.numItemsInspected);
+    return monkeys[0].numItemsInspected * monkeys[1].numItemsInspected;
+}
+
+const startingItemsRegex = / {2}Starting items: (\d+(?:, \d+)*)/;
+const operationRegex = / {2}Operation: new = old ([+*]) (old|\d+)/;
+const testRegex = / {2}Test: divisible by (\d+)/;
+const testTrueRegex = / {4}If true: throw to monkey (\d+)/;
+const testFalseRegex = / {4}If false: throw to monkey (\d+)/;
+
+function parseMonkeyStates(input: string) {
     const states = input.split('\n\n');
-    let modulus = 1;
+    const monkeys: Monkey[] = states.map((state, id) => {
+        const items = state.match(startingItemsRegex)?.[1]
+            .split(', ').map(toInt) ?? [];
 
-    const monkeys: Monkey[] = states.map((state, index) => {
-        const situation = state.split('\n');
-        const modifier = situation[2][OPERATION.length];
-        const by = situation[2].slice(OPERATION.length + 2);
-        const divisibleBy = Number.parseInt(situation[3].slice(situation[3].length - 2), 10);
-        modulus *= divisibleBy;
+        const operationMatch = state.match(operationRegex);
+        const operator = operationMatch?.[1];
+        const modifier = operationMatch?.[2];
+        const modifierInt = toInt(modifier);
 
-        // const items = state.match(startingItemsRegex);
+        const divisibleBy = toInt(state.match(testRegex)?.[1]);
 
         return {
-            id: index,
-            items: situation[1]
-                .slice(START.length)
-                .split(', ')
-                .map((v) => Number.parseInt(v, 10)),
+            id,
+            items,
             operation: (level: number) => {
-                let amount = Number.parseInt(by);
-                if (by === 'old') {
+                let amount = modifierInt;
+                if (modifier === 'old') {
                     amount = level;
                 }
 
-                if (modifier === '+') {
+                if (operator === '+') {
                     return level + amount;
-                } else if (modifier === '*') {
+                } else if (operator === '*') {
                     return level * amount;
                 }
-                throw new Error(`Unknown modifier '${modifier}'`);
+                throw new Error(`Unknown operator '${operator}'`);
             },
             test: {
                 divisibleBy,
-                ifTrue: Number.parseInt(situation[4].slice(situation[4].length - 1), 10),
-                ifFalse: Number.parseInt(situation[5].slice(situation[5].length - 1), 10),
+                trueMonkey: toInt(state.match(testTrueRegex)?.[1]),
+                falseMonkey: toInt(state.match(testFalseRegex)?.[1]),
             },
             numItemsInspected: 0,
         };
     });
-
-    for (let round = 0; round < maxRounds; round++) {
-        for (let turn = 0; turn < monkeys.length; turn++) {
-            const monkey = monkeys[turn];
-            if (monkey.items.length === 0) {
-                continue;
-            }
-
-            monkey.items = monkey.items.reverse();
-            for (let i = monkey.items.length - 1; i >= 0; i--) {
-                const item = monkey.items[i];
-                const { divisibleBy, ifTrue, ifFalse } = monkey.test;
-                monkey.numItemsInspected += 1;
-
-                let level = monkey.operation(item);
-                if (worryDivider) {
-                    level = Math.floor(level / worryDivider);
-                } else {
-                    level = level % modulus;
-                }
-
-                if (level % divisibleBy === 0) {
-                    monkeys[ifTrue].items.push(level);
-                } else {
-                    monkeys[ifFalse].items.push(level);
-                }
-                monkey.items.splice(i, 1);
-            }
-        }
-    }
-
-    monkeys.sort((a, b) => b.numItemsInspected - a.numItemsInspected);
-    const monkeyBusiness = monkeys[0].numItemsInspected * monkeys[1].numItemsInspected;
-
-    return monkeyBusiness;
+    return monkeys;
 }
 
 interface Monkey {
@@ -101,8 +102,12 @@ interface Monkey {
     operation: (level: number) => number;
     test: {
         divisibleBy: number;
-        ifTrue: number;
-        ifFalse: number;
+        trueMonkey: number;
+        falseMonkey: number;
     };
     numItemsInspected: number;
+}
+
+function toInt(value: string = '') {
+    return Number.parseInt(value, 10);
 }
