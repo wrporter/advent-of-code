5513c20b4f349ccec57edb744232448cfe909b32
wesp <wesp@qualtrics.com>
2023-11-11 20:56:50 -0700

    2022 day 10 in go

---
+++ solutions/2022/10/go/solution/solution_test.go
@@ -0,0 +1,206 @@
+package solution
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+var (
+	s     = New()
+	input = s.ReadInputPrefix("../../../../../")
+)
+
+var tests = []struct {
+	input string
+	args1 []interface{}
+	args2 []interface{}
+	want1 interface{}
+	want2 interface{}
+}{
+	{
+		input: `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`,
+		want1: 13140,
+		want2: `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`,
+	},
+	{
+		input: input,
+		want1: 11780,
+		want2: `###..####.#..#.#....###...##..#..#..##..
+#..#....#.#..#.#....#..#.#..#.#..#.#..#.
+#..#...#..#..#.#....###..#..#.#..#.#..#.
+###...#...#..#.#....#..#.####.#..#.####.
+#....#....#..#.#....#..#.#..#.#..#.#..#.
+#....####..##..####.###..#..#..##..#..#.`,
+	},
+}
+
+func TestPart1(t *testing.T) {
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("Test_%d", i), func(t *testing.T) {
+			if got := s.Part1(tt.input, tt.args1...); !reflect.DeepEqual(got, tt.want1) {
+				t.Errorf("Part1() = %v, want %v", got, tt.want1)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("Test_%d", i), func(t *testing.T) {
+			if got := s.Part2(tt.input, tt.args2...); !reflect.DeepEqual(got, tt.want2) {
+				t.Errorf("Part2() = %v, want %v", got, tt.want2)
+			}
+		})
+	}
+}
