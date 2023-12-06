1ee61d86911f319c40c775a7596b00cb5cd05f77
wesp <wesp@qualtrics.com>
2023-12-06 06:31:55 -0700

    Fix total time reporting. It should only be measuring processing time, excluding printing to the console.

--- internal/common/v2/solution/solution.go
+++ internal/common/v2/solution/solution.go
@@ -52,7 +52,6 @@ func (s AbstractSolution) Run(args1 []interface{}, args2 []interface{}) {
 	fmt.Printf("🎄 %s%s%s%d: Day %d\n%s", color.Green, color.Underlined, color.Bold, s.Year, s.Day, color.Reset)
 
 	input := s.ReadInput()
-	startTotal := time.Now()
 
 	start1 := time.Now()
 	answer1 := s.Part1(input, args1...)
@@ -61,13 +60,13 @@ func (s AbstractSolution) Run(args1 []interface{}, args2 []interface{}) {
 	start2 := time.Now()
 	answer2 := s.Part2(input, args2...)
 	elapsed2 := time.Since(start2)
+	elapsedTotal := time.Since(start1)
 
 	padding := ints.Max(len(fmt.Sprintf("%v", answer1)), len(fmt.Sprintf("%v", answer2)))
 
 	fmt.Printf("⭐  %sPart 1: %s%*v%s %s|%s 🕒 %s%s%s\n", color.Green, color.Red, -padding, answer1, color.Reset, color.Cyan, color.Reset, color.Purple, timeit.Round(elapsed1, 2), color.Reset)
 	fmt.Printf("⭐  %sPart 2: %s%*v%s %s|%s 🕒 %s%s%s\n", color.Green, color.Red, -padding, answer2, color.Reset, color.Cyan, color.Reset, color.Purple, timeit.Round(elapsed2, 2), color.Reset)
 
-	elapsedTotal := time.Since(startTotal)
 	fmt.Printf("🕒 %sTotal: %s%s\n", color.Blue, timeit.Round(elapsedTotal, 2), color.Reset)
 }
 
