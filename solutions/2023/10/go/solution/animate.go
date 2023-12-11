36fcc6e6761ac0e45d29c82be4775b014f6bcbfb
wesp <wesp@qualtrics.com>
2023-12-11 14:11:29 -0700

    2023 day 10: Color spots inside and outside the pipes

--- solutions/2023/10/go/solution/animate.go
+++ solutions/2023/10/go/solution/animate.go
@@ -9,14 +9,24 @@ import (
 	"image/color"
 	"log"
 	"math"
+	"strings"
 )
 
 var (
 	gray      = color.RGBA{R: 90, G: 82, B: 85, A: 255}
 	green     = color.RGBA{R: 85, G: 158, B: 131, A: 255}
+	red       = color.RGBA{R: 174, G: 90, B: 65, A: 255}
 	blue      = color.RGBA{R: 27, G: 133, B: 184, A: 255}
 	darkBlue  = color.RGBA{R: 12, G: 23, B: 39, A: 255}
 	fontColor = color.White
+)
+
+type mode int
+
+const (
+	modeStart mode = iota
+	modePart1Done
+	modePart2Done
 )
 
 func Animate() {
@@ -43,6 +53,11 @@ type Game struct {
 	current   *geometry.Point
 	pipe      Pipe
 	loop      map[geometry.Point]string
+
+	inside  map[geometry.Point]bool
+	outside map[geometry.Point]bool
+
+	mode mode
 
 	part1 int
 	part2 int
@@ -73,6 +88,9 @@ func (g *Game) Restart() {
 	g.pipe = g.startPipe
 	g.current = g.start.Copy()
 	g.loop = map[geometry.Point]string{*g.start.Copy(): g.pipe.char}
+	g.mode = modeStart
+	g.inside = make(map[geometry.Point]bool)
+	g.outside = make(map[geometry.Point]bool)
 
 	g.AbstractGame.Restart()
 }
@@ -81,18 +99,42 @@ func (g *Game) Play() {
 	for i := 0; i < g.Speed; i++ {
 		g.step()
 
-		if g.current.Equals(g.start) {
+		if g.mode == modeStart && g.current.Equals(g.start) {
 			g.part1 = len(g.loop) / 2
-			g.Mode = animate.ModeDone
+			g.mode = modePart1Done
 		}
 	}
 }
 
 func (g *Game) step() {
-	g.current.Move(g.pipe.next)
-	char := g.grid[g.current.Y][g.current.X]
-	g.pipe = Pipes[IntoPipe{char, g.pipe.next}]
-	g.loop[*g.current.Copy()] = g.pipe.char
+	if g.mode == modeStart {
+		g.current.Move(g.pipe.next)
+		char := g.grid[g.current.Y][g.current.X]
+		g.pipe = Pipes[IntoPipe{char, g.pipe.next}]
+		g.loop[*g.current.Copy()] = g.pipe.char
+	} else if g.mode == modePart1Done {
+		for _, ray := range getTopAndLeftEdges(g.grid) {
+			intersections := 0
+
+			for ray.Y < len(g.grid) && ray.X < len(g.grid[ray.Y]) {
+				tile := g.grid[ray.Y][ray.X]
+
+				if _, isOnLoop := g.loop[*ray]; isOnLoop && strings.Contains("|-JF", tile) {
+					intersections += 1
+				} else if !isOnLoop {
+					if intersections%2 == 1 {
+						g.inside[*ray.Copy()] = true
+					} else {
+						g.outside[*ray.Copy()] = true
+					}
+				}
+
+				ray.Move(geometry.DownRight)
+			}
+		}
+
+		g.Mode = animate.ModeDone
+	}
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -112,11 +154,17 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	for y := range g.grid {
 		for x := range g.grid[y] {
-			p := geometry.NewPoint(x, y)
-			if _, ok := g.loop[*p]; !ok {
-				g.drawFilledRect(screen, x*g.TileSize+cellBorder, y*g.TileSize+cellBorder, 0, 0, gray)
+			p := *geometry.NewPoint(x, y)
+			if _, ok := g.loop[p]; !ok {
+				clr := gray
+				if g.inside[p] {
+					clr = green
+				} else if g.outside[p] {
+					clr = red
+				}
+				g.drawFilledRect(screen, x*g.TileSize+cellBorder, y*g.TileSize+cellBorder, 0, 0, clr)
 			} else {
-				switch g.loop[*p] {
+				switch g.loop[p] {
 				case "-":
 					g.drawFilledRect(screen, x*g.TileSize, y*g.TileSize+cellBorder, 2*cellBorder, 0, blue)
 				case "|":
