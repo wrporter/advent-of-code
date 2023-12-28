ba61dd014a28c51bad0d58987073d59ec8eefbad
wrporter <wesleyrporter@gmail.com>
2023-12-28 14:17:09 -0700

    2023 day 23: Extract functions

--- src/solutions/2023/23/go/main.go
+++ src/solutions/2023/23/go/main.go
@@ -48,14 +48,26 @@ func part2(input string, _ ...interface{}) interface{} {
 	grid := strings.Split(input, "\n")
 	start := geometry.NewPoint(1, 0)
 	goal := geometry.NewPoint(len(grid[0])-2, len(grid)-1)
+	graph, ids := collapse(grid, start)
 
+	trimmedDistance := graph[ids[*start]].edges[0].distance + graph[ids[*goal]].edges[0].distance
+	startId := trim(graph, ids, start)
+	goalId := trim(graph, ids, goal)
+
+	return dfsRecursive(graph, goalId, startId, 0, 1<<startId) + trimmedDistance
+	//return dfsIterative(graph, goalId, startId) + trimmedDistance
+}
+
+func collapse(grid []string, start *geometry.Point) ([]Node, map[geometry.Point]int) {
 	graph := []Node{{id: 0}}
 	ids := map[geometry.Point]int{*start: 0}
-
 	seen := make(map[geometry.Point]bool)
+	queue := []*geometry.Point{start}
 
-	var collapse func(current *geometry.Point)
-	collapse = func(current *geometry.Point) {
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
 		for _, next := range getEdges(grid, current, current) {
 			if seen[*next] {
 				continue
@@ -84,50 +96,73 @@ func part2(input string, _ ...interface{}) interface{} {
 			ids[*next] = nextId
 			seen[*prev] = true
 
-			collapse(next)
+			queue = append(queue, next)
 		}
 	}
 
-	collapse(start)
+	return graph, ids
+}
 
-	trimmedDistance := 0
-
-	trim := func(current *geometry.Point) int {
-		id := ids[*current]
-		next := graph[ids[*current]].edges[0].id
-		for i, edge := range graph[next].edges {
-			if edge.id == id {
-				graph[next].edges = myslice.Remove(graph[next].edges, i)
-				trimmedDistance += edge.distance
-				graph[id].edges = nil
-				break
-			}
+func trim(graph []Node, ids map[geometry.Point]int, current *geometry.Point) int {
+	id := ids[*current]
+	next := graph[ids[*current]].edges[0].id
+	for i, edge := range graph[next].edges {
+		if edge.id == id {
+			graph[next].edges = myslice.Remove(graph[next].edges, i)
+			break
 		}
-		return next
+	}
+	return next
+}
+
+func dfsRecursive(graph []Node, goalId, current, distance, seen int) int {
+	if current == goalId {
+		return distance
 	}
 
-	startId := trim(start)
-	goalId := trim(goal)
+	longest := 0
 
-	var dfs func(current int, distance int, seen int) int
-	dfs = func(current int, distance int, seen int) int {
-		longest := -1
+	for _, edge := range graph[current].edges {
+		bit := 1 << edge.id
+		if (seen & bit) == 0 {
+			longest = max(longest, dfsRecursive(graph, goalId, edge.id, distance+edge.distance, seen|bit))
+		}
+	}
 
-		if current == goalId {
-			return distance
+	return longest
+}
+
+func dfsIterative(graph []Node, goalId int, startId int) int {
+	stack := []Item{{startId, 0, 1 << startId}}
+	longest := 0
+
+	for len(stack) > 0 {
+		current := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if current.id == goalId {
+			longest = max(longest, current.distance)
 		}
 
-		for _, edge := range graph[current].edges {
+		for _, edge := range graph[current.id].edges {
 			bit := 1 << edge.id
-			if (seen & bit) == 0 {
-				longest = max(longest, dfs(edge.id, distance+edge.distance, seen|bit))
+			if (current.seen & bit) == 0 {
+				stack = append(stack, Item{edge.id, current.distance + edge.distance, current.seen | bit})
 			}
 		}
-
-		return longest
 	}
 
-	return dfs(startId, trimmedDistance, 1<<startId)
+	return longest
+}
+
+type Item struct {
+	id       int
+	distance int
+	seen     int
+}
+
+func (i Item) Less(item aoc.PriorityQueueItem) bool {
+	return i.distance > item.(Item).distance
 }
 
 type Node struct {
--- src/solutions/2023/23/readme.md
+++ src/solutions/2023/23/readme.md
@@ -12,8 +12,18 @@ This one can be solved with a fairly simple depth-first search.
 
 You can still use your same solution from Part 1, mine took 28 minutes to get the result. I let it run while I worked on optimizing by collapsing corridors in the grid via a connected graph.
 
-By pre-constructing a graph of the connected nodes, collapsing the paths between junctions, my runtime was reduced to 4.1 seconds. A huge improvement! I'm sure there are more optimizations I can make. I might come back to this one later to learn more.
+By pre-constructing a graph of the connected nodes, collapsing the paths between junctions, my runtime was reduced to 4.19 seconds. A huge improvement! I'm sure there are more optimizations I can make. I might come back to this one later to learn more.
 
-Other optimizations I made:
+#### Optimization progression
 
-1. Switch edges from a map to a slice. This was nearly a 2x speedup, going from 4.19s to 2.28s.
+1. `28m` - Brute force with original grid (Part 1 solution).
+2. `4.19s` - Collapse paths.
+3. 35d10284 - `2.28s` - Switch edges from a map to a slice. 
+4. c5153e5c - `1.27s` - Switch the graph from a map of points to a slice. Assign each node an index ID. This introduced some minor code complexity by keeping track of and assigning an index ID to each node when constructing the graph.
+5. 3b0e71ff - `211ms` - Switch the seen map to a slice. This was made possible by switching the graph to a list so we could now index directly into a slice of a pre-defined size.
+6. 698e1cf9 - `112ms` - Trim the start and end nodes from the graph and advance the start and end pointers to the first junctions. We can do this because these nodes only have a single edge connection. Trimming them from the graph greatly reduces the backtracking required in the depth-first search.
+7. 8fb1b382 - `105ms` - Use a bitset for tracking visited nodes. We can do this because we have less than 64 nodes, and we specified an ID from 0 to 36 (less for the sample input).
+
+Changes that did not work:
+
+1. I figured that an iterative DFS would be more performant than a recursive one. However, this is not the case. I wonder if the Go compiler is optimizing the function call, particularly since all of the arguments are straight integers now that we use a bitset to track visited nodes. Whereas, the iterative solution is growing and contracting a slice. The iterative solution is `170ms` compared to the `105ms` recursive solution.
