186ea98ca9b0cf2ccbb43d710213685ed9de7b6d
wrporter <wesleyrporter@gmail.com>
2024-12-14 10:54:09 -0700

    2024 day 14

---
+++ src/solutions/2024/14/go/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"aoc/src/lib/go/aoc"
+	"aoc/src/lib/go/convert"
+	"aoc/src/lib/go/ints"
+	"aoc/src/lib/go/v2/geometry"
+	"github.com/samber/lo"
+	"math"
+	"regexp"
+	"strings"
+)
+
+var robotRegex = regexp.MustCompile(`p=(\d+),(\d+) v=(-?\d+),(-?\d+)`)
+
+func part1(input string, args1 ...interface{}) interface{} {
+	robots, width, height := parse(input, args1)
+	return getSafetyFactor(robots, 100, width, height)
+}
+
+func part2(input string, args1 ...interface{}) interface{} {
+	robots, width, height := parse(input, args1)
+	minSafetyFactor := math.MaxInt
+	minTime := math.MaxInt
+
+	for t := 1; t <= 10_000; t++ {
+		safetyFactor := getSafetyFactor(robots, 1, width, height)
+		if safetyFactor < minSafetyFactor {
+			minSafetyFactor = safetyFactor
+			minTime = t
+		}
+		//fmt.Print("\033[H\033[2J")
+		//fmt.Printf("\n\nAfter %d seconds\n", t)
+		//fmt.Println(render(robots, width, height))
+		//time.Sleep(100 * time.Millisecond)
+	}
+
+	//r2, _, _ := parse(input, args1)
+	//getSafetyFactor(r2, minTime, width, height)
+	//fmt.Println(render(r2, width, height))
+	return minTime
+}
+
+func getSafetyFactor(robots []*Robot, time, width, height int) int {
+	quadrants := make([]int, 4)
+
+	for _, r := range robots {
+		r.X = ints.WrapMod(r.X+r.velocity.X*time, width)
+		r.Y = ints.WrapMod(r.Y+r.velocity.Y*time, height)
+
+		if r.Y < height/2 {
+			if r.X < width/2 {
+				quadrants[0]++
+			} else if r.X > width/2 {
+				quadrants[1]++
+			}
+		} else if r.Y > height/2 {
+			if r.X < width/2 {
+				quadrants[2]++
+			} else if r.X > width/2 {
+				quadrants[3]++
+			}
+		}
+	}
+
+	return quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]
+}
+
+func parse(input string, args1 []interface{}) ([]*Robot, int, int) {
+	width, height := args1[0].(int), args1[1].(int)
+	return lo.Map(strings.Split(input, "\n"), func(line string, _ int) *Robot {
+		values := convert.ToIntsV2(robotRegex.FindStringSubmatch(line)[1:])
+		return &Robot{
+			Point:    geometry.NewPoint(values[0], values[1]),
+			velocity: geometry.NewPoint(values[2], values[3]),
+		}
+	}), width, height
+}
+
+func render(robots []*Robot, width, height int) string {
+	grid := make([][]rune, height)
+	for y := range height {
+		grid[y] = make([]rune, width)
+		for x := range width {
+			grid[y][x] = '.'
+		}
+	}
+
+	for _, r := range robots {
+		x, y := r.X, r.Y
+		cell := grid[r.Y][r.X]
+		if cell == '.' {
+			grid[y][x] = '1'
+		} else {
+			grid[y][x] += 1
+		}
+	}
+
+	builder := strings.Builder{}
+	delimiter := ""
+	for _, row := range grid {
+		builder.WriteString(delimiter)
+		builder.WriteString(string(row))
+		delimiter = "\n"
+	}
+	return builder.String()
+}
+
+type Robot struct {
+	*geometry.Point
+	velocity *geometry.Point
+}
+
+func main() {
+	args := []interface{}{101, 103}
+	New().Run(args, args)
+}
+
+func New() aoc.Solution {
+	return aoc.Solution{Year: 2024, Day: 14, Part1: part1, Part2: part2}
+}
---
+++ src/solutions/2024/14/go/main_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+var (
+	s     = New()
+	input = s.ReadInputFromTests()
+)
+
+var tests = []struct {
+	input string
+	args1 []interface{}
+	args2 []interface{}
+	want1 interface{}
+	want2 interface{}
+}{
+	{
+		input: `p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3`,
+		args1: []interface{}{11, 7},
+		args2: []interface{}{11, 7},
+		want1: 12,
+		want2: 5,
+	},
+	{
+		input: input,
+		args1: []interface{}{101, 103},
+		args2: []interface{}{101, 103},
+		want1: 230686500,
+		want2: 7672,
+	},
+}
+
+func Test_Part1(t *testing.T) {
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("Test_%d", i), func(t *testing.T) {
+			if got := s.Part1(tt.input, tt.args1...); !reflect.DeepEqual(got, tt.want1) {
+				t.Errorf("Part1() = %v, want %v", got, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_Part2(t *testing.T) {
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("Test_%d", i), func(t *testing.T) {
+			if got := s.Part2(tt.input, tt.args2...); !reflect.DeepEqual(got, tt.want2) {
+				t.Errorf("Part2() = %v, want %v", got, tt.want2)
+			}
+		})
+	}
+}
+
+func BenchmarkSolution_Part1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s.Part1(input)
+	}
+}
+
+func BenchmarkSolution_Part2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s.Part2(input)
+	}
+}
